6d9fd1d05ddfc3b9a1a4895916c6ec54085d4109
ying <932126394@qq.com>
2021-03-01 17:29:35 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -4,18 +4,28 @@ import router from "./router";
 import store from "./store";
 import mixins from "./mixins";
 import MintUI from "mint-ui";
+// import Viewer from 'viewerjs';
+
 
 import "normalize.css/normalize.css";
 import "mint-ui/lib/style.css";
-
+// import 'viewerjs/dist/viewer.css';
 
 import "./sheet/app.scss";
 import "font-awesome/scss/font-awesome.scss"
-// import "lib-flexible/flexible.js";
+
 
 Vue.config.productionTip = false;
 Vue.mixin(mixins);
 Vue.use(MintUI);
+
+// Viewer.setDefaults({
+//   Options: { 'inline': true, 'button': true, 'navbar': true, 'title': true, 'toolbar': true, 'tooltip': true, 'movable': true, 'zoomable': true, 'rotatable': true, 'scalable': true, 'transition': true, 'fullscreen': true, 'keyboard': true, 'url': 'data-source' }
+// })
+// Vue.use(Viewer);
+
+
+
 new Vue({
   router,
   store,
--- src/views/attachContent.vue
+++ src/views/attachContent.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="attach-content">
-      attach-content
+    <viewer :images="descImgs" class="descimgBox">
+      <img v-for="(decImg, index) in descImgs" :key="index" :src="decImg" />
+    </viewer>
   </div>
 </template>
+
+<script>
+import "viewerjs/dist/viewer.css";
+// import Viewer from "viewerjs";
+
+export default {
+  data() {
+    return {
+      descImgs: [
+        "https://tse1-mm.cn.bing.net/th?id=OIP.fLI-fIeiAEMZwLhz6KkcMQAAAA&w=201&h=200&c=7&o=5&pid=1.7",
+        "https://tse1-mm.cn.bing.net/th?id=OIP.fLI-fIeiAEMZwLhz6KkcMQAAAA&w=201&h=200&c=7&o=5&pid=1.7",
+        "https://tse1-mm.cn.bing.net/th?id=OIP.fLI-fIeiAEMZwLhz6KkcMQAAAA&w=201&h=200&c=7&o=5&pid=1.7",
+        "https://tse1-mm.cn.bing.net/th?id=OIP.fLI-fIeiAEMZwLhz6KkcMQAAAA&w=201&h=200&c=7&o=5&pid=1.7",
+        "https://tse1-mm.cn.bing.net/th?id=OIP.fLI-fIeiAEMZwLhz6KkcMQAAAA&w=201&h=200&c=7&o=5&pid=1.7",
+      ],
+    };
+  },
+};
+</script>
 <style lang="scss">
 .attach-content {
   position: absolute;
